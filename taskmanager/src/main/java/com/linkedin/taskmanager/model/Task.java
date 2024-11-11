73e0fb7f082fb7a0aab613f7ec35890ac0f4386f
maaike <maaike@maaike.nl>
2024-11-11 12:24:31 +0100

    red

---
+++ taskmanager/src/main/java/com/linkedin/taskmanager/model/Task.java
@@ -0,0 +1,20 @@
+package com.linkedin.taskmanager.model;
+
+public class Task {
+
+    public Long getId() {
+        return 0L;
+    }
+
+    public String getTitle() {
+        return "";
+    }
+
+    public void setTitle(String title) {
+
+    }
+
+    public void setDescription(String description) {
+
+    }
+}
---
+++ taskmanager/src/test/java/com/linkedin/taskmanager/repository/TaskRepositoryTest.java
@@ -0,0 +1,27 @@
+package com.linkedin.taskmanager.repository;
+
+import com.linkedin.taskmanager.model.Task;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+public class TaskRepositoryTest {
+    private TaskRepository taskRepository;
+    @Test
+    void testSaveTask() {
+        // arrange
+        Task task = new Task();
+        task.setTitle("Test Task");
+        task.setDescription("This is test task");
+
+        // act
+        Task savedTask = taskRepository.save(task);
+
+        // assert
+        assertNotNull(savedTask.getId());
+        assertEquals("Test Task", savedTask.getTitle());
+    }
+}

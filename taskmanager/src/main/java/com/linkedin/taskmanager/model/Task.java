d52827d9656de8c6cfca34e360e0331791538c29
maaike <maaike@maaike.nl>
2024-11-15 17:51:22 +0100

    test save task

---
+++ taskmanager/src/main/java/com/linkedin/taskmanager/model/Task.java
@@ -0,0 +1,13 @@
+package com.linkedin.taskmanager.model;
+
+public class Task {
+    public void setTitle(String title) {}
+
+    public String getTitle() {
+        return null;
+    }
+    public void setStatus(String status) {}
+
+
+
+}
---
+++ taskmanager/src/test/java/com/linkedin/taskmanager/repository/TaskRepositoryTest.java
@@ -0,0 +1,30 @@
+package com.linkedin.taskmanager.repository;
+
+import com.linkedin.taskmanager.model.Task;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+public class TaskRepositoryTest {
+
+    @Autowired
+    TaskRepository taskRepository;
+
+    @Test
+    void testSaveTask() {
+        // arrange
+        Task task = new Task();
+        task.setTitle("Test task");
+        task.setStatus("To do");
+
+        // act
+        Task savedTask = taskRepository.save(task);
+
+        // assert
+        assertNotNull(savedTask);
+        assertEquals("Test task", savedTask.getTitle());
+    }
+}

418bcacb5962462a42dfd27942ec359be91f3d08
maaike <maaike@maaike.nl>
2024-11-16 15:27:22 +0100

    test get all tasks

--- taskmanager/src/test/java/com/linkedin/taskmanager/controller/TaskControllerTest.java
+++ taskmanager/src/test/java/com/linkedin/taskmanager/controller/TaskControllerTest.java
@@ -7,9 +7,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
+
 
 import java.util.Arrays;
 import java.util.List;
@@ -26,15 +31,20 @@ public class TaskControllerTest {
     private ObjectMapper objectMapper;
 
     @Test
-    void testGetAllTasks() {
+    void testGetAllTasks() throws Exception {
         // arrange
         List<Task> tasks = Arrays.asList(
                 new Task("Task 1", "To do"),
                 new Task("Task 2", "In progress")
         );
 
-//        when(taskService.getA)
-        // act & assert
+        when(taskService.getAllTasks()).thenReturn(tasks);
 
+        // act & assert
+        mockMvc.perform(get("/tasks").contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(2)))
+                .andExpect(jsonPath("$[0].title").value("Task 1"))
+                .andExpect(jsonPath("$[1].title").value("Task 2"));
     }
 }

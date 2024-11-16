188fe83779f7f584491d7debe0c08867a2c3e1bd
maaike <maaike@maaike.nl>
2024-11-16 11:49:56 +0100

    unit test

--- taskmanager/src/main/java/com/linkedin/taskmanager/service/TaskService.java
+++ taskmanager/src/main/java/com/linkedin/taskmanager/service/TaskService.java
@@ -1,4 +1,9 @@
 package com.linkedin.taskmanager.service;
 
+import com.linkedin.taskmanager.model.Task;
+
 public class TaskService {
+    public Task updateTaskStatus(Long id, String status){
+        return null;
+    }
 }

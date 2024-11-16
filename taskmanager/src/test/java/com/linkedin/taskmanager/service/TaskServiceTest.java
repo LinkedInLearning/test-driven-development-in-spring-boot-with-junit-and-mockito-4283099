aca7c8dfe8e26a6edf4f932d249009efac961303
maaike <maaike@maaike.nl>
2024-11-16 11:41:05 +0100

    init mockito

---
+++ taskmanager/src/test/java/com/linkedin/taskmanager/service/TaskServiceTest.java
@@ -0,0 +1,19 @@
+package com.linkedin.taskmanager.service;
+
+import com.linkedin.taskmanager.repository.TaskRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class TaskServiceTest {
+    @Mock
+    private TaskRepository taskRepository;
+
+    @InjectMocks
+    private TaskService taskService;
+
+}
